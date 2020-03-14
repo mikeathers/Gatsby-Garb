0397ce945c3d49ebe3f8a3f37d6d17fcb45b0b22
Mike Atherton <athers_05@hotmail.co.uk>
2020-03-14 07:51:13 +0000

    Added private product feature to auth users

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
 import Img from 'gatsby-image'
+import netlifyIdentity from 'netlify-identity-widget'
 
 export const query = graphql`
   query {
@@ -12,6 +13,7 @@ export const query = graphql`
           slug
           name
           price
+          private
           image {
             fluid(maxWidth: 800) {
               ...GatsbyContentfulFluid
@@ -23,31 +25,59 @@ export const query = graphql`
   }
 `
 
-const Products = ({ data: { allContentfulProduct } }) => (
-  <Layout>
-    <div>
-      <h2>Garb Products</h2>
-      {/* Products List */}
-      {allContentfulProduct.edges.map(({ node: product }) => (
-        <div key={product.id}>
-          <Link
-            to={`/products/${product.slug}`}
-            style={{ textDecoration: 'none', color: '#551a8b' }}
-          >
-            <h3>
-              {product.name} -{' '}
-              <span
-                style={{ fontSize: '1.2rem', fontWeight: 300, color: '#f60' }}
+class Products extends React.Component {
+  state = {
+    products: [],
+  }
+  componentDidMount() {
+    this.getProducts()
+    netlifyIdentity.on('login', user => this.getProducts(user))
+    netlifyIdentity.on('logout', () => this.getProducts())
+  }
+
+  getProducts = user => {
+    console.log(user)
+    const allProducts = this.props.data.allContentfulProduct.edges
+    const products =
+      netlifyIdentity.currentUser() !== null
+        ? allProducts
+        : allProducts.filter(({ node: product }) => !product.private)
+    this.setState({ products })
+  }
+
+  render() {
+    const { products } = this.state
+    return (
+      <Layout>
+        <div>
+          <h2>Garb Products</h2>
+          {/* Products List */}
+          {products.map(({ node: product }) => (
+            <div key={product.id}>
+              <Link
+                to={`/products/${product.slug}`}
+                style={{ textDecoration: 'none', color: '#551a8b' }}
               >
-                £{product.price}
-              </span>
-            </h3>
-          </Link>
-          <Img style={{ maxWidth: 600 }} fluid={product.image.fluid} />
+                <h3>
+                  {product.name} -{' '}
+                  <span
+                    style={{
+                      fontSize: '1.2rem',
+                      fontWeight: 300,
+                      color: '#f60',
+                    }}
+                  >
+                    £{product.price}
+                  </span>
+                </h3>
+              </Link>
+              <Img style={{ maxWidth: 600 }} fluid={product.image.fluid} />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </Layout>
-)
+      </Layout>
+    )
+  }
+}
 
 export default Products
